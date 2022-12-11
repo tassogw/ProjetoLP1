846c4c03bd234be5ffe84f745aef7771af1a57ad
Tasso <tassogwdev@gmail.com>
2022-12-11 18:26:23 -0300

    Game polished
    List of changes:
    - Commented unused import;
    - Added path to check empty strings to player name;
    - Removed redundant game ending checks;
    - Changes to comments in general;
    - Added tied game check in verifyGameEnd method;
    - Changes to polish "Both shot" outputs.

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 import assets.Enemy;
 import assets.Player;
-
-import java.sql.SQLOutput;
+//import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,7 +10,7 @@ public class Main {
         Player player = new Player();
         Enemy enemy = new Enemy();
         Scanner sc = new Scanner(System.in);
-        String playerName = "";
+        String playerName;
         boolean gameEnd = false, validAction = true;
         int round = 1; //Round output
         int action = 0, random = 0, damage = 30;
@@ -20,7 +19,13 @@ public class Main {
         System.out.println("--- Welcome to Java's Western! ---");
         System.out.println("Insert your name:");
         playerName = sc.nextLine();
-        player.setName(playerName.trim());
+        //Checking if string is empty
+        if (!(playerName.trim().equals(""))) {
+            playerName = playerName.trim(); //If not, set name normally
+        } else {
+            playerName = "Player"; //If it is, set name to "Player"
+        }
+        player.setName(playerName);
         System.out.println(); //Skip a line
 
         //enemy.setIsLoaded(false);
@@ -54,7 +59,8 @@ public class Main {
             //Compare results
             //Same actions
             if (player.getAction() == enemy.getAction() && player.getAction() == 0) {
-                //Both shot
+            //Both shot
+                System.out.println("Both shot...");
                 //Check who got shot (one, both, or none)
                 random = randomAction(4);
                 switch (random) {
@@ -63,9 +69,6 @@ public class Main {
                         player.gotShot(damage);
                         //Show player's hp and skip a line
                         printGunslingerHealths(player, enemy, 0);
-                        if (verifyGameEnd(player, enemy)) {
-                            gameEnd = true;
-                        }
                         break;
                     case 1:
                         //Enemy got shot
@@ -85,77 +88,86 @@ public class Main {
                         break;
                     case 3:
                         //None got shot
-                        System.out.println("Both shot and missed.");
+                        System.out.println("but missed.");
                         System.out.println(); //Skip a line
                         break;
                 }
-                if (verifyGameEnd(player, enemy)) {
-                    gameEnd = true;
-                }
 
-            } else if (player.getAction() == enemy.getAction() && player.getAction() == 1) { //Both reloaded
+            } else if (player.getAction() == enemy.getAction() && player.getAction() == 1) {
+            //Both reloaded
                 System.out.println("Both reloaded.");
                 //Reload guns
                 player.reloadGun();
                 enemy.reloadGun();
                 System.out.println(); //Skip a line
-            } else if (player.getAction() == enemy.getAction() && player.getAction() == 2) { //Both dodged
+            } else if (player.getAction() == enemy.getAction() && player.getAction() == 2) {
+            //Both dodged
                 System.out.println("Both dodged.");
                 System.out.println(); //Skip a line
             } else if (player.getAction() != enemy.getAction() && player.getAction() == 0 && enemy.getAction() == 1) {
+            //Player shot while enemy was reloading
                 enemy.gotShot(damage);
                 printGunslingerHealths(player, enemy, 1);
             } else if (player.getAction() != enemy.getAction() && player.getAction() == 1 && enemy.getAction() == 0) {
+            //Enemy shot while player was reloading
                 player.gotShot(damage);
                 printGunslingerHealths(player, enemy, 0);
-                if (verifyGameEnd(player, enemy)) {
-                    gameEnd = true;
-                }
             } else if (player.getAction() != enemy.getAction() && player.getAction() == 0 && enemy.getAction() == 2) {
+            //Player shot and enemy tried to dodge
                 random = randomAction(2);
                 switch (random) {
                     case 0:
                         //Enemy got shot
                         enemy.gotShot(damage);
                         //Show enemy's hp and skip a line
+                        System.out.println("Enemy's dodge failed.");
                         printGunslingerHealths(player, enemy, 1);
                         break;
                     case 1:
-                        System.out.printf("Player Shot.%nEnemy dodged.%n");
+                        //Enemy dodged
+                        System.out.printf("%s Shot.%nEnemy dodged.%n", player.getName());
+                        //Show enemy's hp and skip a line
                         System.out.printf("Enemy's health: %d%n%n", enemy.getHealth());
                         break;
                 }
-                if (verifyGameEnd(player, enemy)) {
-                    gameEnd = true;
-                }
             } else if (player.getAction() != enemy.getAction() && player.getAction() == 2 && enemy.getAction() == 0) {
+            //Enemy shot and player tried to dodge
                 random = randomAction(2);
                 switch (random) {
                     case 0:
                         //Player got shot
                         player.gotShot(damage);
                         //Show player's hp and skip a line
+                        System.out.printf("%s's dodge failed.%n", player.getName());
                         printGunslingerHealths(player, enemy, 0);
                         break;
                     case 1:
-                        System.out.printf("Enemy shot.%nPlayer dodged.%n");
+                        //Player dodged
+                        System.out.printf("Enemy shot.%n%s dodged.%n", player.getName());
+                        //Show player's hp and skip a line
                         System.out.printf("%s's health: %d%n%n", player.getName(), player.getHealth());
                         break;
                 }
-                if (verifyGameEnd(player, enemy)) {
-                    gameEnd = true;
-                }
             } else if (player.getAction() != enemy.getAction() && player.getAction() == 1 && enemy.getAction() == 2) {
+            //Enemy dodged and player reloaded
                 System.out.printf("Enemy dodged.%n");
                 player.reloadGun();
-                System.out.printf("%s's reloaded.%n", player.getName());
+                System.out.printf("%s reloaded.%n", player.getName());
 
             } else if (player.getAction() != enemy.getAction() && player.getAction() == 2 && enemy.getAction() == 1) {
+            //Player dodged and enemy reloaded
                 System.out.printf("Enemy reloaded.%n");
                 enemy.reloadGun();
-                System.out.printf("%s's dodged.%n", player.getName());
+                System.out.printf("%s dodged.%n", player.getName());
 
             }
+
+            //Check if game ended
+            if (verifyGameEnd(player, enemy)) {
+                gameEnd = true;
+            }
+
+            //Round update
             round++;
         } while (!gameEnd);
     }
@@ -171,10 +183,13 @@ public class Main {
 
     public static boolean verifyGameEnd(Player player, Enemy enemy) {
         if (enemy.getHealth() == 0 && player.getHealth() > 0) {
-            System.out.printf("Game Ended. Winner is: %s !", player.getName());
+            System.out.printf("Game Over. Winner is: %s!%n", player.getName());
             return true;
         } else if (enemy.getHealth() > 0 && player.getHealth() == 0) {
-            System.out.printf("Game Ended. Winner is: Enemy ", player.getName());
+            System.out.println("Game Over. Winner is: Enemy.");
+            return true;
+        } else if (enemy.getHealth() == player.getHealth() && player.getHealth() == 0) {
+            System.out.println("Game Over. It's a tie.");
             return true;
         } else {
             return false;
@@ -190,12 +205,12 @@ public class Main {
             System.out.printf("Enemy got shot.%n");
             System.out.printf("Enemy's health: %d%n%n", enemy.getHealth());
         } else if (valueToBePrinted == 2) {
-            System.out.printf("Both shot each other.%n");
-            System.out.printf("%s's health: %d%n Enemy's health: %d%n%n", player.getName(),
+            System.out.printf("and hit.%n");
+            System.out.printf("%s's health: %d%nEnemy's health: %d%n%n", player.getName(),
                     player.getHealth(), enemy.getHealth());
         } else {
             System.out.println("Both shot each other.");
-            System.out.printf("%s's health: %d%n Enemy's health: %d%n%n", player.getName(),
+            System.out.printf("%s's health: %d%nEnemy's health: %d%n%n", player.getName(),
                     player.getHealth(), enemy.getHealth());
         }
     }
